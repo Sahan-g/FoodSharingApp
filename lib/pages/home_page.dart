baecdc191530bc94c7f1c7eadee4919c1364d474
Sahan-g <129595975+Sahan-g@users.noreply.github.com>
2024-01-08 19:12:49 +0530

    login ui

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,10 +9,36 @@ class HomePage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: AppColors.background,
-        title: Text("Food Share"),
+        title: Text("Food For All"),
         centerTitle: false,
       ),
-      body: Center(child: Text("Hello this is home page")),
+      body: Center(
+        child: ListView(
+          children: mockRestaurnatsList(),
+        ),
+      ),
     );
   }
+}
+
+Widget _foodItem() {
+  return Row(children: [
+    Image.asset(
+      'assets/images/logo.png',
+      height: 40,
+      width: 40,
+    ),
+    const Text("Restaurant Name"),
+    const SizedBox(
+      width: 8,
+    ),
+  ]);
+}
+
+List<Widget> mockRestaurnatsList() {
+  List<Widget> restaurnts = [];
+  for (int i = 0; i < 20; i++) {
+    restaurnts.add(_foodItem());
+  }
+  return restaurnts;
 }
